7b57116aab9229f015f9916a62bbe96f8bb7b542
Felix Gnass <fgnass@gmail.com>
2022-02-08 16:12:11 +0100

    fix: treat undefined booleans as false

--- example.tsx
+++ example.tsx
@@ -3,37 +3,47 @@ import ReactDOM from "react-dom";
 import { styled } from "./src/react";
 
 const Button = styled("button", {
-  base: "rounded-md px-5 py-2 text-white",
+  base: "px-5 py-2 text-white disabled:bg-gray-400 disabled:text-gray-300",
   variants: {
     color: {
-      neutral: "bg-gray-500",
-      accent: "bg-teal-500",
+      neutral: "bg-slate-500 hover:bg-slate-400",
+      accent: "bg-teal-500 hover:bg-teal-400",
     },
     outlined: {
       true: "border-2",
     },
+    rounded: {
+      true: "rounded-full",
+      false: "rounded-sm",
+    },
   },
   compoundVariants: [
     {
-      variants: {
-        color: "accent",
-        outlined: true,
-      },
-      className: "border-teal-800",
+      variants: { color: "accent", outlined: true },
+      className: "border-teal-600",
     },
   ],
+  defaultVariants: {
+    color: "neutral",
+  },
 });
 
 function App() {
   return (
-    <>
-      <Button color="neutral" onClick={console.log}>
-        Neutral
+    <div className="flex justify-center items-center pt-8 gap-4 flex-wrap">
+      <Button color="accent" onClick={console.log}>
+        Accent
       </Button>
-      <Button color="accent" outlined onClick={console.log}>
+      <Button color="neutral" rounded>
+        Neutral + Rounded
+      </Button>
+      <Button color="accent" outlined>
         Accent + Outlined
       </Button>
-    </>
+      <Button color="accent" disabled>
+        Disabled
+      </Button>
+    </div>
   );
 }
 
--- src/index.ts
+++ src/index.ts
@@ -23,24 +23,32 @@ type StringToBool<T> = T extends "true" | "false" ? boolean : T;
 export function variants<V extends Variants>({
   base,
   variants,
-  compoundVariants,
-  defaultVariants,
+  compoundVariants = [],
+  defaultVariants = {},
 }: VariantsConfig<V>) {
+  const isBooleanVariant = (name: string) => {
+    const v = variants?.[name];
+    return v && ("false" in v || "true" in v);
+  };
+
   return (props: VariantSelection<V>) => {
     const res = [base];
 
     const getSelected = (name: string) =>
-      props[name] ?? defaultVariants?.[name];
+      props[name] ??
+      defaultVariants[name] ??
+      (isBooleanVariant(name) ? false : undefined);
 
     for (let name in variants) {
       const selected = getSelected(name);
-      const options: any = variants[name];
-      if (selected) res.push(options[selected]);
+      if (selected !== undefined) res.push(variants[name][selected.toString()]);
     }
-    for (let { variants, className } of compoundVariants ?? []) {
-      const isSelected = (name: string) => getSelected(name) == variants[name];
-      const allSelected = Object.keys(variants).every(isSelected);
-      if (allSelected) res.push(className);
+
+    for (let { variants, className } of compoundVariants) {
+      const isSelected = (name: string) => getSelected(name) === variants[name];
+      if (Object.keys(variants).every(isSelected)) {
+        res.push(className);
+      }
     }
     return res.filter(Boolean).join(" ");
   };
